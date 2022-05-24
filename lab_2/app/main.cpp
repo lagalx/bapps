376b893fe0a10ba25f54c4ebc4627f97d7da48f6
lagalx <lagal.work@gmail.com>
2022-05-24 13:53:33 +0300

    Remove dark theme palette

--- lab_2/app/main.cpp
+++ lab_2/app/main.cpp
@@ -92,29 +92,10 @@ bool isAppPatched() {
   return false;
 }
 
-QPalette getPalette() {
-  auto palette = QPalette();
-  palette.setColor(QPalette::Window, QColor(53, 53, 53));
-  palette.setColor(QPalette::WindowText, Qt::white);
-  palette.setColor(QPalette::Base, QColor(25, 25, 25));
-  palette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
-  palette.setColor(QPalette::ToolTipBase, Qt::black);
-  palette.setColor(QPalette::ToolTipText, Qt::white);
-  palette.setColor(QPalette::Text, Qt::white);
-  palette.setColor(QPalette::Button, QColor(53, 53, 53));
-  palette.setColor(QPalette::ButtonText, Qt::white);
-  palette.setColor(QPalette::BrightText, Qt::red);
-  palette.setColor(QPalette::Link, QColor(42, 130, 218));
-  palette.setColor(QPalette::Highlight, QColor(42, 130, 218));
-  palette.setColor(QPalette::HighlightedText, Qt::black);
-  return palette;
-}
-
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
 
   a.setStyle("Fusion");
-  a.setPalette(getPalette());
 
   MainWindow w;
   w.show();
